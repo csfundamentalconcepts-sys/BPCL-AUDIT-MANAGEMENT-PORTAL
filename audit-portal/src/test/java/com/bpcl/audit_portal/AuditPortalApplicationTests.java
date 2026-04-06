f4380b67edb43ac8dccbcc889f61570773116074
SATYAM PRAKASH SINGH(सत्यम प्रकाश सिंह) <satyamprakashsi@corp.bharatpetroleum.com>
2026-04-06 11:11:53 +0530

    first commit

---
+++ audit-portal/src/test/java/com/bpcl/audit_portal/AuditPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpcl.audit_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
